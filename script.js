d2c0c81aaa8146f2e4ba2579be3c71e9508af6f3
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-10 01:29:02 +0000

    Jun 10, 2026, 1:29 AM

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ console.log("Running project Neural DSP site")
 
     function buyArchetypeGojira() {
     const item = document.getElementById("buyArchetypeGojira").value;
-    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");    
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    
+    if (item == "256") {
+        OUTPUT.innerHTML += "You have entered the correct amount of Money";
     }
+
+    }
+    
  }
 
