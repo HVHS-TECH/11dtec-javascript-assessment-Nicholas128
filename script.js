f7710bb392989c5082fb6acc9f0872bada31e2fe
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-15 22:37:15 +0000

    Jun 15, 2026, 10:37 PM

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ function showForm() {
     
 
     if (choice == "256") {
-    OUTPUT.innerHTML = "You have entered the correct amount of money $" +choice;
+    OUTPUT.innerHTML = "You have entered the correct amount of money $" +choice+"<br>";
     console.log("Correct amount of money has been entered");
     } 
     else if (choice != "256") {
-    OUTPUT.innerHTML = "Incorrect amount of money entered $"+choice;
+    OUTPUT.innerHTML = "Incorrect amount of money entered $"+choice+"<br>";
     console.log("Incorrect amount of money has been entered");
     }
 
