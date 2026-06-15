237012e11a683879653fceedb6d334e9a4422bcf
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-15 22:50:08 +0000

    Jun 15, 2026, 10:50 PM

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ function showForm() {
 
 
 function buyArchetypeGojira() {
-    const choice = document.getElementById("buyArchetypeGojiraField").value;
+    const choice = document.getElementById("moneyField").value;
     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
     var number = "256";
+    var number = MONEY_FIELD.value;
     
 
-    if (number === "256") {
+    if (number == "256") {
         OUTPUT.innerHTML = "You have entered the correct amount of money";
         console.log("Correct amount of money has been entered");
     } else {
