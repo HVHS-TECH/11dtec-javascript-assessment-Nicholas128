9545ef75cbf5125a5b7e2857d74a3a311523d589
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-25 03:05:12 +0000

    Jun 25, 2026, 3:05 AM

--- script.js
+++ script.js
@@ -67,48 +67,31 @@ function getPocketMoneyInput() {
 }
 
 function getFormInputPetrucci() {
-   const money = document.getElementById("moneyFieldPetrucci").value;
-    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");    
+    const money = document.getElementById("moneyFieldPetrucci").value;
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
     if (money == "296") {
-        OUTPUT.innerHTML += "<div class='positionText11'><p>You have sucessfully purched Archetype Petrucci for $" + money + "</p></div>";
-        console.log("Correct amount of money has been entered");
+        OUTPUT.innerHTML += "<div class='positionText11'><p>You have successfully purchased Archetype Petrucci for $" + money + "</p></div>";
     } else {
-        OUTPUT.innerHTML += "<div class='positionText11'><div class='textStyle'><p>Incorrect amount of money entered </p></div></div>";
-        console.log("Incorrect amount of money has been entered");
+        OUTPUT.innerHTML += "<div class='positionText11'><div class='textStyle'><p>Incorrect amount of money entered</p></div></div>";
     }
+}
 
-   function CountDownDays2() {
-    const item = document.getElementById("countDownDays")
+function CountDownDays2() {
     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-    var now = new Date().getTime();
-    const endDate = new Date().getTime() + (14 * 24 * 60 * 60 * 1000);
-    }
+}
 
-
-      function showForm3() {
+function showForm3() {
     document.getElementById("buyArchetypeAbasiField").style.display = "block";
-    console.log("Displaying form")  
 }
-    function getFormInputAbasi() {
-   const money = document.getElementById("moneyFieldAbasi").value;
-    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");    
 
-    if (money == "236") {     
-        OUTPUT.innerHTML += "<div class='positionText12'><p>You have sucessfully purched Archetype Abasi for $" + money + "</p></div>";
-        console.log("Correct amount of money has been entered");
+function getFormInputAbasi() {
+    const money = document.getElementById("moneyFieldAbasi").value;
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+    if (money == "236") {
+        OUTPUT.innerHTML += "<div class='positionText12'><p>You have successfully purchased Archetype Abasi for $" + money + "</p></div>";
     } else {
-        OUTPUT.innerHTML += "<div class='positionText12'><div class='textStyle'><p>Incorrect amount of money entered </p></div></div>";
-        console.log("Incorrect amount of money has been entered");
+        OUTPUT.innerHTML += "<div class='positionText12'><div class='textStyle'><p>Incorrect amount of money entered</p></div></div>";
     }
-
 }
-
-
-}
-
-  
-
-
-
-    
