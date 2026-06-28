1eeed822861e1947f14d10aaacdf383ada60a258
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-28 21:14:05 +0000

    Jun 28, 2026, 9:14 PM

--- script.js
+++ script.js
@@ -49,7 +49,8 @@ var countDownDate = new Date("14d").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-
+  // Get today's date and time
+  var now = new Date().getTime();
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
