cf830ea9a95ce49d7f25bf2dfddc9c95014359f4
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-28 21:25:47 +0000

    Jun 28, 2026, 9:25 PM

--- script.js
+++ script.js
@@ -44,38 +44,37 @@ function calculateChange(_money, _price) {
 
 function countDownDays(){
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-
-
+// Set the date we're counting down to
+var countDownDate = new Date();
+countDownDate.setDate(countDownDate.getDate() + 14);
+countDownDate = countDownDate.getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
-var countDownDate = new Date();
-countDownDate.setDate(countDownDate.getDate() + 14);
-countDownDate = countDownDate.getTime();
-
+  var now = new Date().getTime();
+    
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-
+    
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
- OUTPUT.innerHTML += days + "d " + hours + "h "
+    
+  // Output the result in an element with id="demo"
+ OUTPUT.innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
+    
+  // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    OUTPUT.innerHTML += "EXPIRED";
+    OUTPUT.innerHTML = "EXPIRED";
   }
 }, 1000);
 
 }
-
     
 
  
