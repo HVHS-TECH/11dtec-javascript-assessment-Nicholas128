1f82af96c3781c9ca9de393184db5d4d927f65a6
Nicholas128 <24049nm@hvhs.school.nz>
2026-06-29 22:59:56 +0000

    30.6.26

--- script.js
+++ script.js
@@ -43,10 +43,11 @@ function calculateChange(_money, _price) {
 }
 
 function countDownDays(){
+
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-var countDownDate = new Date();
 countDownDate.setDate(countDownDate.getDate() + 14);
 countDownDate = countDownDate.getTime();
+
 var x = setInterval(function() {
 var now = new Date().getTime();
 var distance = countDownDate - now;
@@ -89,21 +90,38 @@ OUTPUT.innerHTML = "<div class='positionText15'> <p>" +days + "d " + hours + "h 
 
   }
   function showForm2() {
-    document.getElementById("buyArchetypePetrucciField").style.display = "block";
-    console.log("Displaying form")  
+       document.getElementById("pocketMoneyForm2").style.display = "block";
+    console.log("Displaying 2nd form")  
 }
 
-function getFormInputPetrucci() {
-    const money = document.getElementById("moneyFieldPetrucci").value;
+function getPocketMoneyInput2() {
+    console.log("getPocketMoneyInput2 works");
+    const POCKETMONEY_FIELD = document.getElementById("pocketMoneyInput2");
+    const userPocketMoney = Number(POCKETMONEY_FIELD.value);
     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    const archetypePetrucciPrice = 296;
+    OUTPUT.innerHTML += "<div class='positionText17'><p>Your amount is $" + userPocketMoney + "</div></p>";
+         
+    if (userPocketMoney >= archetypePetrucciPrice) {
+        var change = calculateChange(userPocketMoney, archetypePetrucciPrice);
 
-    if (money == "296") {
-        OUTPUT.innerHTML += "<div class='positionText11'><p>You have successfully purchased Archetype Petrucci for $" + money + "</p></div>";
+        OUTPUT.innerHTML += "<p><div class='positionText18'>You will get $" + change + " change</p></div>";
+
     } else {
-        OUTPUT.innerHTML += "<div class='positionText11'><div class='textStyle'><p>Incorrect amount of money entered</p></div></div>";
+        OUTPUT.innerHTML += "<p><div class='positionText18'>You can't afford Archetype Petrucci</p></div>";
     }
 }
 
+function calculateChange(_money, _price) {
+
+    var change = _money - _price;
+
+    return change;
+    var change = calculateChange(userPocketMoney, 296);
+
+    OUTPUT.innerHTML += "<div class='positionText7'><p>You will get $" + change + " change</p></div>";
+
+}
 function CountDownDays2() {
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 var countDownDate = new Date();
@@ -121,22 +139,64 @@ OUTPUT.innerHTML = "<div class='positionText16'> <p>" +days + "d " + hours + "h 
 
   if (distance < 0) {
     clearInterval(x);
-    OUTPUT.innerHTML = "Buy Archetype Gojira";
+    OUTPUT.innerHTML = "Buy Archetype Petrucci";
   }
 }, 1000);
 }
 
 function showForm3() {
-    document.getElementById("buyArchetypeAbasiField").style.display = "block";
+document.getElementById("pocketMoneyForm3").style.display = "block";
+console.log("Displaying 3nd form")      
 }
 
-function getFormInputAbasi() {
-    const money = document.getElementById("moneyFieldAbasi").value;
+function getPocketMoneyInput3() {
+    console.log("getPocketMoneyInput3 works");
+    const POCKETMONEY_FIELD = document.getElementById("pocketMoneyInput3");
+    const userPocketMoney = Number(POCKETMONEY_FIELD.value);
     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    const archetypeAbasiPrice = 236;
+    OUTPUT.innerHTML += "<div class='positionText19'><p>Your amount is $" + userPocketMoney + "</div></p>";
+         
+    if (userPocketMoney >= archetypeAbasiPrice) {
+        var change = calculateChange(userPocketMoney, archetypeAbasiPrice);
 
-    if (money == "236") {
-        OUTPUT.innerHTML += "<div class='positionText12'><p>You have successfully purchased Archetype Abasi for $" + money + "</p></div>";
+        OUTPUT.innerHTML += "<p><div class='positionText20'>You will get $" + change + " change</p></div>";
+
     } else {
-        OUTPUT.innerHTML += "<div class='positionText12'><div class='textStyle'><p>Incorrect amount of money entered</p></div></div>";
+        OUTPUT.innerHTML += "<p><div class='positionText22'>You can't afford Archetype Abasi</p></div>";
     }
 }
+
+function calculateChange(_money, _price) {
+
+    var change = _money - _price;
+
+    return change;
+    var change = calculateChange(userPocketMoney, 236);
+
+    OUTPUT.innerHTML += "<div class='positionText7'><p>You will get $" + change + " change</p></div>";
+
+}
+
+function CountDownDays3() {
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+var countDownDate = new Date();
+countDownDate.setDate(countDownDate.getDate() + 14);
+countDownDate = countDownDate.getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+OUTPUT.innerHTML = "<div class='positionText21'> <p>" +days + "d " + hours + "h " + minutes + "m " + seconds + "s <p></div>";
+
+  if (distance < 0) {
+    clearInterval(x);
+    OUTPUT.innerHTML = "Buy Archetype Abasi";
+  }
+}, 1000);
+}
+
