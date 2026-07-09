443a190424f7a48907356df7e12b35834828d3f3
Nicholas128 <24049nm@hvhs.school.nz>
2026-07-09 23:41:15 +0000

    Jul 9, 2026, 11:41 PM

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function addToCart() {
 
 function checkOut() {
   const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-  OUTPUT.innerHTML += "<div>Checking out...</div>";
+  <a href="checkOut.js"></a>
   console.log("Checking out");
 }
 
