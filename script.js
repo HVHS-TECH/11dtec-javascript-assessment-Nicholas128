77bdf418db35db9add2e6a8389a3d807bff00553
Nicholas128 <24049nm@hvhs.school.nz>
2026-07-14 02:06:21 +0000

    Jul 14, 2026, 2:06 AM

--- script.js
+++ script.js
@@ -109,9 +109,7 @@ function addToCart3() {
   OUTPUT.innerHTML += "<div class='positionText25'>You have just added " + item + " to your cart</div><br><br>";
   console.log("You have just added " + item + " to your cart");
 
-  if (!document.getElementById("checkOutButton")) {
-    OUTPUT.innerHTML += "<div class='button10'><button id='checkOutButton'>Check Out</button><div class='city'></a></div></div>";
-  }
+
 }
  
    
